cbf6f1f83a5f1d9254bb5615fbf4176d75143ebb
abdul basit <abdulbasit2745@gmail.com>
2025-08-03 15:50:18 +0500

    change

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 "use strict";
-console.log("i am a student of generative and agentic AI");
+console.log("console.log kaya ha");
+console.log("console.log ik function ha jo javascript/typescrips main output dainy k liye kam krta ha");
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
-console.log ("i am a student of generative and agentic AI");
+console.log ("console.log kaya ha");
+console.log ("console.log ik function ha jo javascript/typescrips main output dainy k liye kam krta ha")
